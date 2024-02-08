6517d52c117340b037075aa09cc7a9420837be7c
Joseph Hammerman <josephhammerman@datadoghq.com>
2024-02-08 07:34:08 -0800

    Added some info about shifting into local deve mode

--- app/controllers/index.go
+++ app/controllers/index.go
@@ -3,8 +3,24 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+        //"regexp"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	// Uncomment the following to enable local development
+	// WebRTC functions require https
+        // httpProtocolMatched, err := regexp.Match(`localhost`, []byte(r.Host))
+
+	//if err != nil {
+        //   internalError(err, w)
+        //  return
+        //}
+
+       //if httpProtocolMatched {
+       //    http.Redirect(w, r, fmt.Sprintf("http://%s/home/", r.Host), http.StatusFound)
+       //} else {
+       //    http.Redirect(w, r, fmt.Sprintf("https://%s/home/", r.Host), http.StatusFound)
+       //}
+
 	http.Redirect(w, r, fmt.Sprintf("https://%s/home/", r.Host), http.StatusFound)
 }
