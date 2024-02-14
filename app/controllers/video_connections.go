390ac9399d7fa0634aeb816759000e831b913e83
Joseph Hammerman <josephhammerman@datadoghq.com>
2024-02-14 09:30:00 -0800

    Remove extraneous read function. Make write logic work correctly :fingers crossed:

--- app/controllers/video_connections.go
+++ app/controllers/video_connections.go
@@ -24,11 +24,15 @@ func VideoConnections(w http.ResponseWriter, r *http.Request) {
     peers := []string{userID, peerID}
     sort.Strings(peers)
     topicName := fmt.Sprintf("video-%s-%s", peers[0], peers[1])
-    
-    //peerToWSMap[topicName] = make(map[string]interface{})
-    peerToWSMap[topicName] = map[string]interface{}{
-        userID: nil,
-        peerID: nil,
+
+    _, ok := peerToWSMap[topicName]
+
+    if !ok {
+        log.Printf("Topic created", topicName) 
+        peerToWSMap[topicName] = map[string]interface{}{
+            userID: nil,
+            peerID: nil,
+        }
     }
 
     ctx := context.Background()
@@ -58,44 +62,19 @@ func wsLoop(ctx context.Context, cancelFunc context.CancelFunc, ws *websocket.Co
 
 func pubSubLoop(cctx, ctx context.Context, ws *websocket.Conn, peerToWSMap map[string]map[string]interface{}, peerID string, userID string, topicName string) {
     log.Printf("Starting pubSubLoop for %s...", userID)
-    // subscriptionName := fmt.Sprintf("%s-%s", userID, topic.ID())
-    // sub := pubSub.Subscription(subscriptionName)
-    // if exists, err := sub.Exists(ctx); err != nil {
-    //     log.Printf("Error checking if sub exists: %s", err)
-    //     return
-    // } else if !exists {
-    //    log.Printf("Creating subscription: %s", subscriptionName)
-    //    if _, err = pubSub.CreateSubscription(
-    //        context.Background(),
-    //        subscriptionName,
-    //        pubsub.SubscriptionConfig{
-    //            Topic:                 topic,
-    //            EnableMessageOrdering: true,
-    //        },
-    //    ); err != nil {
-    //        log.Printf("Error creating subscription: %s", err)
-    //        return
-    //    }
-    //}
-    //if err := sub.Receive(cctx, func(c context.Context, m *pubsub.Message) {
     
-    //for topic := range peerToWSMap {
-    //if topicName == peerToWSMap[topicName] 
     for peer := range peerToWSMap[topicName] {
-       if _, message, err := ws.Read(ctx); err != nil {
-         if peer != peerID {
+        if peer != peerID {
              log.Println("skipping message from self")
              return
          }
          log.Printf("Received message to publish")
-         if err := ws.Write(ctx, websocket.MessageText, message); err != nil {
+         if err := ws.Write(ctx, websocket.MessageText, peerToWSMap[topicName][peer].([]byte)); err != nil {
              log.Printf("Error writing message to %s: %s", userID, err)
              return
          }
          log.Printf("Shutting down pubSubLoop for %s...", userID)
-        }
     }
-    //}
 }
 
 func closeWS(ws *websocket.Conn) {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/josephhammerman1979/josephhammerman.com
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/mux v1.8.1
 	github.com/kisielk/errcheck v1.4.0 // indirect
 	github.com/mgechev/revive v1.0.2 // indirect
 	github.com/stretchr/testify v1.6.1 // indirect
