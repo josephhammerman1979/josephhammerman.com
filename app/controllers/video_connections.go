c0461201cfba24a9a0d71026ca2aba80e74f482b
Joseph Hammerman <josephhammerman@datadoghq.com>
2024-02-20 11:20:33 -0800

    Passing messages as an array of structs with sender defined as an attribute, lets see

--- app/controllers/video_connections.go
+++ app/controllers/video_connections.go
@@ -8,10 +8,15 @@ import (
     "nhooyr.io/websocket"
     "sort"
     "strings"
-    "time"
+    //"time"
 )
 
-var peerToWSMap = make(map[string]map[string]interface{})
+type rTCMessage struct {
+	sender    string
+        data      []byte
+}
+
+var topicMessage = make(map[string][]rTCMessage)
 
 func VideoConnections(w http.ResponseWriter, r *http.Request) {
     ws, err := websocket.Accept(w, r, nil)
@@ -26,64 +31,68 @@ func VideoConnections(w http.ResponseWriter, r *http.Request) {
     sort.Strings(peers)
     topicName := fmt.Sprintf("video-%s-%s", peers[0], peers[1])
 
-    _, ok := peerToWSMap[topicName]
+    ctx := context.Background()
+
+    _, ok := topicMessage[topicName]
 
     if !ok {
-        log.Printf("Topic created %s", topicName) 
-        peerToWSMap[topicName] = map[string]interface{}{
-            userID: nil,
-            peerID: nil,
-        }
+        log.Printf("Creating topic %s...", topicName)
+        var messages []rTCMessage
+        topicMessage[topicName] = messages
     }
-
-    ctx := context.Background()
 
     //cctx, cancelFunc := context.WithCancel(ctx)
 
-    wsLoop(ctx, ws, peerToWSMap, userID, topicName)
-    pubSubLoop(ctx, ws, peerToWSMap, userID, peerID, topicName)
+    wsLoop(ctx, ws, topicMessage, userID, topicName)
+    pubSubLoop(ctx, ws, topicMessage, userID, peerID, topicName)
 }
 
-func wsLoop(ctx context.Context, ws *websocket.Conn, peerToWSMap map[string]map[string]interface{}, userID string, topicName string) {
+func wsLoop(ctx context.Context, ws *websocket.Conn, topicMessage map[string][]rTCMessage, userID string, topicName string) {
     log.Printf("Starting wsLoop for %s...", userID)
     for {
-        if _, message, err := ws.Reader(ctx); err != nil {
+        if _, message, err := ws.Read(ctx); err != nil {
             // could check for 'close' here and tell peer we have closed
             log.Printf("Error reading message %s", err)
             break
         } else {
             log.Printf("Received message to websocket.")
-            peerToWSMap[topicName][userID] = message
-                return
+            newMessage := rTCMessage{
+                sender: userID,
+                data: message,
+            }
+            topicMessage[topicName] = append(topicMessage[topicName], newMessage)
+
+            return
         }
     }
     // cancelFunc()
     log.Printf("Shutting down wsLoop for %s...", userID)
 }
 
-func pubSubLoop(ctx context.Context, ws *websocket.Conn, peerToWSMap map[string]map[string]interface{}, userID string, peerID string, topicName string) {
-    log.Printf("Starting pubSubLoop for %s...", userID)
-    
-    for peer := range peerToWSMap[topicName] {
-        for {
-            if peer != peerID {
+func pubSubLoop(ctx context.Context, ws *websocket.Conn, topicMessage map[string][]rTCMessage, userID string, peerID string, topicName string) {
+    log.Printf("Starting pubSubLoop for %s, topic %s...", userID, topicName)
+
+    _, ok := topicMessage[topicName]
+
+    if !ok {
+        log.Printf("Topic not exist in map %s, erroring", topicName)
+        return
+    }    
+
+    for {
+        for message := range topicMessage[topicName] {
+            if topicMessage[topicName][message].sender == userID {
                 log.Println("skipping message from self")
                 return
-             }
-             _, ok := peerToWSMap[topicName][peerID].([]byte) 
-             if !ok {
-                 time.Sleep(500 * time.Second)
-                 log.Println("waiting for peer")
-                 continue
-             } else {
-                 if err := ws.Write(ctx, websocket.MessageText, peerToWSMap[topicName][peer].([]byte)); err != nil {
-                     log.Printf("Error writing message to %s: %s", peerID, err)
-                     log.Printf("Shutting down pubSubLoop for %s...", userID)
-                     break
-                 } // else {
-                   //  log.Printf("Received message to publish")
-                   //  continue
-                 // }
+            } else {
+                if err := ws.Write(ctx, websocket.MessageText, topicMessage[topicName][message].data); err != nil {
+                    log.Printf("Error writing message %s", err)
+                    log.Printf("Shutting down pubSubLoop for %s...", userID)
+                    break
+                } // else {
+                  //  log.Printf("Received message to publish")
+                  //  continue
+                // }
             } 
         }
     }
