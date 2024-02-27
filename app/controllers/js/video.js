f562b7c048497b5e1d39fa7462144f23fd5ff37d
Joseph Hammerman <josephhammerman@datadoghq.com>
2024-02-27 10:02:35 -0800

    The other ICE servers I added were all non-responsive, let's remove cruft and figure out the real error

--- app/controllers/js/video.js
+++ app/controllers/js/video.js
@@ -4,11 +4,6 @@ let peerConnection = new RTCPeerConnection({
   iceServers: [
     {urls: [
         "stun:stun.l.google.com:19302",
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302",
-        "stun:stun.stunprotocol.org:3478",
       ],
     }
   ],
