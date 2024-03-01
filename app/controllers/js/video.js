47004130f138724cbb014a82931244c2efa43946
Joseph Hammerman <josephhammerman@datadoghq.com>
2024-03-01 11:57:59 -0800

    dsjnf;knzd

--- app/controllers/js/video.js
+++ app/controllers/js/video.js
@@ -9,7 +9,7 @@ async function initializePeerConnection() {
     // const iceServers = await response.json();
 
     // const peerConfiguration = { iceServers: iceServers, iceCandidatePoolSize: 10 };
-    peerConnection = new RTCPeerConnection({"iceServers": [{"urls": "stun:stun.l.google.com:19302", iceCandidatePoolSize: 10]})
+    peerConnection = new RTCPeerConnection({"iceServers": [{"urls": "stun:stun.l.google.com:19302", iceCandidatePoolSize: 10 }]})
 
     // Now that we have the ICE servers, create the peer connection
     // peerConnection = new RTCPeerConnection(peerConfiguration);
